191a158b761cbf05fee100829f5650510208ddcf
Tommaso Goni <tommasogo99@gmail.com>
2021-08-22 17:45:23 +0200

    Added UuidToByte16() to Helper.java

--- src/main/java/com.github.tommyt0mmy.drugdealing/utility/Helper.java
+++ src/main/java/com.github.tommyt0mmy.drugdealing/utility/Helper.java
@@ -1,11 +1,17 @@
 package com.github.tommyt0mmy.drugdealing.utility;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
 public class Helper
 {
     private static DrugType[] drugTypes;
     private static CriminalRole[] criminalRoles;
 
-    static {
+    static
+    {
         drugTypes = new DrugType[DrugType.values().length];
         for (DrugType drugType : DrugType.values())
             drugTypes[drugType.getId()] = drugType;
@@ -20,8 +26,18 @@ public class Helper
         return criminalRoles[id];
     }
 
+    public static byte[] UuidToByte16(@NotNull UUID uuid)
+    {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(new byte[16]);
+        byteBuffer.putLong(uuid.getMostSignificantBits());
+        byteBuffer.putLong(uuid.getLeastSignificantBits());
+
+        return byteBuffer.array();
+    }
+
     public static DrugType getDrugType(int id)
     {
         return drugTypes[id];
     }
+
 }
