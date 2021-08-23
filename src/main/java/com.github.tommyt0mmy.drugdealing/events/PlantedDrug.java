fec059a072abbf6cd68449657649e2537a5c69c9
Tommaso Goni <tommasogo99@gmail.com>
2021-08-23 16:14:42 +0200

    Removed deprecated methods, fixed minor bugs, wg bug needs to me tested

--- src/main/java/com.github.tommyt0mmy.drugdealing/events/PlantedDrug.java
+++ src/main/java/com.github.tommyt0mmy.drugdealing/events/PlantedDrug.java
@@ -1,77 +1,96 @@
 package com.github.tommyt0mmy.drugdealing.events;
 
 import com.github.tommyt0mmy.drugdealing.DrugDealing;
+import com.github.tommyt0mmy.drugdealing.utility.DrugType;
 import com.github.tommyt0mmy.drugdealing.utility.Permissions;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Optional;
 
 public class PlantedDrug implements Listener
 {
 
     private final DrugDealing instance = DrugDealing.getInstance();
 
-    @EventHandler
+    // TODO || THIS CLASS CAUSES A BUG WITH WORLDGUARD TURNED ON, BLOCKS CAN BE PLACED EVERYWHERE, TESTS NEED TO BE DONE BEFORE RELEASE
+
+    @EventHandler(priority = EventPriority.LOW) //todo maybe priority helps with the worldguard bug, needs testing
     public void onPlantedDrug(BlockPlaceEvent e)
     {
-        Player p = (Player) e.getPlayer();
+        Player p = e.getPlayer();
         try
         {
-
-            Block placed = (Block) e.getBlock();
-
-            e.setCancelled(true); //TODO THIS CAUSES TOO THE WORLDGUARD BUG MAYBE
+            Block placedBlock = e.getBlock();
 
             ItemStack placedIS = p.getInventory().getItemInMainHand();
+            Optional<DrugType> drugTypeOptional = instance.drugs.getDrugType(placedIS);
 
-            //Coke placed
-            //TODO FIX REDUNDANCY
-            if (instance.drugs.isCokePlantItemStack(placedIS))
+            //todo fix redundant code
+
+            //Coke planted
+
+            if (drugTypeOptional.isPresent() && drugTypeOptional.get() == DrugType.COKE_PLANT)
             {
                 //permissions check
-                if (!(p.hasPermission(Permissions.getPermission("plant_coke")) || p.hasPermission(Permissions.getPermission("plant_weed"))))
+                if (!p.hasPermission(Permissions.getPermission("plant_coke")))
                 {
-                    e.setCancelled(true);
                     p.sendMessage(instance.language.formattedChatMessage("invalid_permission"));
+
+                    e.setCancelled(true);
                     return;
                 }
 
-                if (instance.drugs.isPlantedOnFarmland(placed))
+                //plants need to be planted on farmland
+                if (instance.drugs.isPlantedOnFarmland(placedBlock))
                 {
                     p.sendMessage(instance.language.formattedChatMessage("planted_coke"));
-                    instance.plantsRegister.addPlant(placed, "coke");
+                    instance.plantsRegister.addPlant(placedBlock.getLocation(), DrugType.COKE_PLANT);
                 } else
                 {
                     p.sendMessage(instance.language.formattedChatMessage("invalid_surface"));
+
+                    e.setCancelled(true);
                     return;
                 }
             }
 
             //Weed planted
 
-            if (instance.drugs.isWeedPlantItemStack(placedIS))
+            if (drugTypeOptional.isPresent() && drugTypeOptional.get() == DrugType.WEED_PLANT)
             {
-                if (instance.drugs.isPlantedOnFarmland(placed))
+                //permissions check
+                if (!p.hasPermission(Permissions.getPermission("plant_weed")))
+                {
+                    p.sendMessage(instance.language.formattedChatMessage("invalid_permission"));
+
+                    e.setCancelled(true);
+                    return;
+                }
+
+                //plants need to be planted on farmland
+                if (instance.drugs.isPlantedOnFarmland(placedBlock))
                 {
                     p.sendMessage(instance.language.formattedChatMessage("planted_weed"));
-                    instance.plantsRegister.addPlant(placed, "weed");
+                    instance.plantsRegister.addPlant(placedBlock.getLocation(), DrugType.WEED_PLANT);
                 } else
                 {
                     p.sendMessage(instance.language.formattedChatMessage("invalid_surface"));
+
+                    e.setCancelled(true);
                     return;
                 }
             }
 
-            e.setCancelled(false); //TODO MAYBE THIS IS THE CAUSE OF THE WORLDGUARD BUG
         } catch (Exception exception)
         {
-            e.setCancelled(true);
             p.sendMessage(instance.language.formattedChatMessage("unexpected_error"));
             exception.printStackTrace();
-            return;
         }
     }
 }
